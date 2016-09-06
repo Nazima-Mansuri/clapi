fe2c65cb25e9bd5445267206100ffab50eb7e165
Arun Saxena <arun130469@gmail.com>
2016-09-06 13:28:45 +0530

    whats happening

--- src/main/java/com/brewconsulting/DB/User.java
+++ src/main/java/com/brewconsulting/DB/User.java
@@ -1,51 +1,41 @@
 package com.brewconsulting.DB;
 
-import com.brewconsulting.*;
+import com.brewconsulting.DB.common.DBConnectionProvider;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
-
-import java.sql.Array;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
 
 import javax.ws.rs.NotAuthorizedException;
 
-import org.apache.catalina.tribes.util.Arrays;
-
 public class User {
+
+	@JsonProperty("clientId")
 	public int clientId; // -1 means no client
+
+	@JsonProperty("id")
 	public int id;
+
+	@JsonProperty("username")
 	public String username;
+
+	@JsonProperty("schemaName")
 	public String schemaName;
+
+	@JsonProperty("firstName")
 	public String firstName;
+
+	@JsonProperty("lastName")
 	public String lastName;
+
+	@JsonProperty("roles")
 	public ArrayList<Irole> roles;
-	public String addLine1;
-	public String addLine2;
-	public String addLine3;
-	public String city;
-	public String state;
-	public String[] phones;
-	public String designation;
-	public int divId;
-	public String divName;
-	public int empNumber;
-	public Date createDate;
-	public int createdBy;
-	public Date updateDate;
-	public int updateBy;
 
 	public interface Irole {
-		public int roleid = 0;
-		public String rolename = null;
 	}
 
 	// make the constructor private.
@@ -92,7 +82,8 @@ public class User {
 				}
 			}
 		} finally {
-			con.close();
+			if (con != null)
+				con.close();
 		}
 
 		return user;
@@ -123,14 +114,14 @@ public class User {
 					user = new User();
 					user.id = id;
 					user.clientId = masterUsers.getInt(2);
-					user.schemaName = masterUsers.getString(3);
-					user.firstName = masterUsers.getString(4);
-					user.lastName = masterUsers.getString(5);
+					user.schemaName = masterUsers.getString(5);
+					user.firstName = masterUsers.getString(3);
+					user.lastName = masterUsers.getString(4);
 					user.username = masterUsers.getString(8);
 					user.roles = new ArrayList<Irole>();
 					user.roles.add(new Irole() {
-						public int roleid = masterUsers.getInt(6);
-						public String rolename = masterUsers.getString(7);
+						public int id = masterUsers.getInt(6);
+						public String name = masterUsers.getString(7);
 					});
 
 					continue;
@@ -138,13 +129,14 @@ public class User {
 
 				if (user.roles != null) {
 					user.roles.add(new Irole() {
-						public int roleid = masterUsers.getInt(6);
-						public String rolename = masterUsers.getString(7);
+						public int id = masterUsers.getInt(6);
+						public String name = masterUsers.getString(7);
 					});
 				}
 			}
 		} finally {
-			con.close();
+			if (con != null)
+				con.close();
 		}
 		return user;
 
@@ -166,6 +158,7 @@ public class User {
 		Iterator<JsonNode> it = rolesNode.elements();
 		UserProfile user = null;
 
+		// check if the user has permission to see others profile.
 		if (loggedInUser.get("id").asInt() != id) {
 			boolean isAllowed = false;
 
@@ -179,9 +172,8 @@ public class User {
 
 			if (!isAllowed)
 				throw new NotAuthorizedException("");
-
+			
 			user = new UserProfile(find(id));
-			System.out.println("client id" + user.clientId);
 		} else {
 			user = new UserProfile();
 			user.id = loggedInUser.get("id").asInt();
@@ -189,56 +181,70 @@ public class User {
 			user.schemaName = loggedInUser.get("schemaName").asText();
 			user.firstName = loggedInUser.get("firstName").asText();
 			user.lastName = loggedInUser.get("lastName").asText();
+			user.username = loggedInUser.get("username").asText();
 			user.roles = new ArrayList<Irole>();
 			while (it.hasNext()) {
 				final JsonNode roleNode = it.next();
 				user.roles.add(new Irole() {
-					public int roleid = roleNode.get("roleid").asInt();
-					public String rolename = roleNode.get("rolename").asText();
+					public int id = roleNode.get("roleid").asInt();
+					public String name = roleNode.get("rolename").asText();
 				});
 			}
 		} // else
 
+		// admin users will not have their profiles filled with any other
+		// details as they are not in
+		// any firm DB.
 		if (!isUserAdmin(user))
 			fillProfileInfo(user);
-		
-		
+
 		return user;
 	}
 
 	private static void fillProfileInfo(UserProfile user) throws ClassNotFoundException, SQLException {
-		 Connection con = DBConnectionProvider.getConn();
+		Connection con = DBConnectionProvider.getConn();
+		System.out.println("Schema"+user.schemaName);
 		try {
 			PreparedStatement stmt = con.prepareStatement(
 					"select (address).addLine1 line1, (address).addLine2 line2, (address).addLine3 line3, "
-							+ "(address).city city, (address).state, (address).phone phones, designation, a.divId, "
-							+ "empNumber, b.name," + "a.createDate, a.createBy, a.updateDate, a.updateBy from "
-							+ user.schemaName + ".userProfile a, " + user.schemaName
-							+ ".divisions b where userId = ? and " + "a.divId = b.Id");
+							+ "(address).city city, (address).state, (address).phone phones, designation, a.divId divid, "
+							+ "empNumber, b.name divname," + "a.createDate cdate, a.createBy cby, "
+							+ "a.updateDate  udate,  a.updateBy uby from " + user.schemaName + ".userProfile a, "
+							+ user.schemaName + ".divisions b where userId = ? and " + "a.divId = b.Id");
 			stmt.setInt(1, user.id);
 
 			final ResultSet schemaUsers = stmt.executeQuery();
 			if (schemaUsers != null) {
 				schemaUsers.next();
 				user.addLine1 = schemaUsers.getString("line1");
-				Array arr = schemaUsers.getArray("phones");
-
-				String[] str = (String[]) arr.getArray();
-				System.out.println(str[0] + str[1]);
+				user.addLine2 = schemaUsers.getString("line2");
+				user.addLine3 = schemaUsers.getString("line3");
+				user.city = schemaUsers.getString("city");
+				user.state = schemaUsers.getString("state");
+				user.phones = (String[]) schemaUsers.getArray("phones").getArray();
+				user.designation = schemaUsers.getString("designation");
+				user.divId = schemaUsers.getInt("divid");
+				user.divName = schemaUsers.getString("divname");
+				user.empNum = schemaUsers.getString("empnumber");
+				user.createDate = schemaUsers.getDate("cdate");
+				user.createBy = schemaUsers.getInt("cby");
+				user.updateDate = schemaUsers.getDate("cdate");
+				user.updateBy = schemaUsers.getInt("cby");
 			}
 
 		} finally {
-			con.close();
+			if (con != null)
+				con.close();
 		}
-
 
 	}
 
-	private static boolean isUserAdmin(UserProfile user) {
-		for (Irole role : user.roles) {
-			if (role.roleid == Permissions.ROLE_ROOT)
+	private static boolean isUserAdmin(UserProfile user)
+			throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
+		for (Irole irole : user.roles)
+			if (irole.getClass().getDeclaredField("id").getInt(irole) == Permissions.ROLE_ROOT)
 				return true;
-		}
+
 		return false;
 	}
 
